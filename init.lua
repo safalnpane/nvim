4cc3bae1812bfc13446a9f9f9fe9b38f8d65649d
Safal Neupane <safal@Safals-MBP.lan>
2023-04-05 18:23:01 +1000

    initial

---
+++ init.lua
@@ -0,0 +1,138 @@
+print("Hi Safal! have a great day")
+
+-- EDITING
+-- ================================================
+-- Tabs & Shifts
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- No Swapfile
+vim.opt.swapfile = false
+
+-- Search
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+
+-- Clipboard
+vim.opt.clipboard:append { 'unnamedplus' }
+
+
+-- PLUGINGS
+-- ===============================================
+require("packer").startup(function(use)
+	use { "wbthomason/packer.nvim" }
+	use { "ellisonleao/gruvbox.nvim" }
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use {
+	  'nvim-lualine/lualine.nvim',
+	   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+	use { "fatih/vim-go" }
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+  		requires = {
+		{'neovim/nvim-lspconfig'},             -- Required
+		{'williamboman/mason.nvim'},           -- Optional
+		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+		{'hrsh7th/nvim-cmp'},         -- Required
+		{'hrsh7th/cmp-nvim-lsp'},     -- Required
+		{'hrsh7th/cmp-buffer'},       -- Optional
+		{'hrsh7th/cmp-path'},         -- Optional
+		{'saadparwaiz1/cmp_luasnip'}, -- Optional
+		{'hrsh7th/cmp-nvim-lua'},     -- Optional
+		{'L3MON4D3/LuaSnip'},             -- Required
+		{'rafamadriz/friendly-snippets'}, -- Optional
+  	},
+	use {"akinsho/toggleterm.nvim", tag = '*' },
+	use "terrortylor/nvim-comment",
+}
+end)
+
+-- GRUVBOX
+require("gruvbox").setup({
+	contrast = "hard",
+	palette_overrides = {
+		gray = "#2ea542",
+	}
+})
+
+-- LUALINE
+require("lualine").setup{
+	options = {
+		icons_enabled = false,
+		theme = "onedark",
+		component_separators = "|",
+		section_separators = "",
+	},
+}
+
+-- LSP
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+	"gopls",
+	"pylsp",
+})
+
+lsp.set_preferences({
+	sign_icons = {}
+})
+
+lsp.on_attach(function(client, bufnr)
+	local opts = {buffer = bufnr, remap = false}
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+end)
+
+lsp.setup()
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+		signs = false,
+		virtual_text = true,
+		underline = false,
+	}
+)
+
+-- COMMENT
+require("nvim_comment").setup({
+	operator_mapping = "<leader>/"
+})
+
+-- TERMINAL SETUP
+require("toggleterm").setup{
+	direction = "horizontal",
+}
+
+
+-- KEYMAPS
+-- ================================================
+-- Leader key
+vim.g.mapleader = ","
+
+-- Splits
+vim.keymap.set('n', 'ss', ':split<Return><C-w>w')
+vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+vim.keymap.set('n', '<Space>', '<C-w>w')
+vim.keymap.set('n', 'sh', '<C-w>h')
+vim.keymap.set('n', 'sk', '<C-w>k')
+vim.keymap.set('n', 'sj', '<C-w>j')
+vim.keymap.set('n', 'sl', '<C-w>l')
+
+-- File finder (Telescope)
+vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files)
+
+-- Terminal (ToogleTerm)
+vim.keymap.set('n', '<leader>t', ':ToggleTerm<Return>')
+vim.keymap.set('t', '<esc>', '<C-\\><C-n>')
+
+
+-- LOOK & FEEL
+-- ================================================
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.cmd("colorscheme gruvbox")
